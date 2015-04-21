cf70b19e2d164471e1e43273f10dd9b2f83e26c3
Raúl López <raullopez@siani.es>
2015-04-21 11:04:07 +0100

    Deploy: support generate plugin

--- deploy/bitbucket.sh
+++ deploy/bitbucket.sh
@@ -9,5 +9,6 @@ password=`xmllint --xpath "string(//password)" local/bitbucket-settings.xml`
 source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../engine/target/itrules-$version.jar
 source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../reader-itr/target/itrules-itr-reader-$version.jar
 source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../reader-json/target/itrules-json-reader-$version.jar
+source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../deploy/tmp/zip/itrules-plugin-$version.zip
 
 rm -f cookies.txt > /dev/null
