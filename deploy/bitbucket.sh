eaa198f3ff29ad49640cc19b9bd9963b1ef3d8a9
Raúl López <raullopez@siani.es>
2015-04-21 09:26:46 +0100

    Deploy: support generate plugin

--- deploy/bitbucket.sh
+++ deploy/bitbucket.sh
@@ -3,11 +3,11 @@
 source ./lib/git.sh
 
 version=$(get_last_release)
-user=`xmllint --xpath "string(//user)" bitbucket-settings.xml`
-password=`xmllint --xpath "string(//password)" bitbucket-settings.xml`
+user=`xmllint --xpath "string(//user)" local/bitbucket-settings.xml`
+password=`xmllint --xpath "string(//password)" local/bitbucket-settings.xml`
 
 source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../engine/target/itrules-$version.jar
 source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../reader-itr/target/itrules-itr-reader-$version.jar
 source ./utils/upload-to-bitbucket.sh $user $password siani/itrules/downloads ../reader-json/target/itrules-json-reader-$version.jar
 
-
+rm -f cookies.txt > /dev/null
--- deploy/maven.sh
+++ deploy/maven.sh
@@ -80,7 +80,7 @@ function generate_artifact {
   
   mv dist.pom ../$1/dist.pom
   cd ../$1
-  mvn clean deploy -f dist.pom -s ../deploy/maven-settings.xml
+  mvn clean deploy -f dist.pom -s ../deploy/local/maven-settings.xml
   rm dist.pom
   cd ../deploy
 }
