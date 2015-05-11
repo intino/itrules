4b08990d96670ebfcde1ea97133a2f424a4a36d6
Raúl López <raullopez@siani.es>
2015-05-11 13:23:12 +0100

    Deploy: added snapshot support for jetbrains

--- deploy/jetbrains.sh
+++ deploy/jetbrains.sh
@@ -2,7 +2,7 @@
 
 source ./lib/git.sh
 
-version=$(get_last_release)
+version=$(get_stable_release)
 user=`xmllint --xpath "string(//user)" local/jetbrains-settings.xml`
 password=`xmllint --xpath "string(//password)" local/jetbrains-settings.xml`
 plugin_id=`xmllint --xpath "string(//idplugin)" local/jetbrains-settings.xml`
