31fccb99500ea94e4f89e5dd881f461ab87d8925
Jose Juan Hernandez <josejuanhernandez@siani.es>
2015-05-11 13:51:21 +0100

    Fixed mark separator Added tests

--- engine/src/org/siani/itrules/model/Mark.java
+++ engine/src/org/siani/itrules/model/Mark.java
@@ -23,68 +23,70 @@
 package org.siani.itrules.model;
 
 public class Mark extends AbstractMark {
-	private static final String NewLine = "\n";
-	private static final String OptionSeparator = "+";
+    private static final String NewLine = "\n";
+    private static final String OptionSeparator = "+";
 
-	private String name;
-	private String[] options;
-	private String separator = null;
+    private String name;
+    private String[] options;
+    private String separator = null;
 
-	public Mark(String name, String... options) {
-		this.name = name;
-		this.options = options;
-	}
+    public Mark(String name, String... options) {
+        this.name = name;
+        this.options = options;
+    }
 
     public Mark multiple(String separator) {
         this.separator = separator;
         return this;
     }
 
-	@Override
-	public String fullName() {
-		return name + optionsText();
-	}
+    @Override
+    public String fullName() {
+        return name + optionsText();
+    }
 
-	private String optionsText() {
-		String result = "";
-		for (String option : options) result += OptionSeparator + option;
-		return result;
-	}
+    private String optionsText() {
+        String result = "";
+        for (String option : options) result += OptionSeparator + option;
+        return result;
+    }
 
-	@Override
-	public String name() {
-		return name;
-	}
+    @Override
+    public String name() {
+        return name;
+    }
 
-	@Override
-	public String separator() {
-		return separator;
-	}
+    @Override
+    public String separator() {
+        return separator;
+    }
 
-	@Override
-	public boolean isMultiple() {
-		return separator != null;
-	}
+    @Override
+    public boolean isMultiple() {
+        return separator != null;
+    }
 
-	@Override
-	public String[] options() {
-		return options;
-	}
+    @Override
+    public String[] options() {
+        return options;
+    }
 
-	@Override
-	public String indentation() {
-		return prevToken() instanceof Literal ? calculateIndentation(prevToken()) : "";
-	}
+    @Override
+    public String indentation() {
+        return prevToken() instanceof Literal ? calculateIndentation(prevToken().as(Literal.class)) : "";
+    }
 
-	private String calculateIndentation(Token token) {
-		int i = token.as(Literal.class).literal().lastIndexOf(NewLine);
-		if (i < 0) return "";
-		String[] split = ((Literal) token).literal().substring(i).split(NewLine);
-		return split.length > 1 ? split[1] : "";
-	}
+    private String calculateIndentation(Literal literal) {
+        int index = literal.text().lastIndexOf(NewLine);
+        return index >= 0 ? calculateIndentation(literal.text().substring(index + 1) + '|') : "";
+    }
 
-	@Override
-	public String toString() {
-		return "[" + name + (isMultiple() ? "List" : "") + "]";
-	}
+    private String calculateIndentation(String text) {
+        return text.substring(0, text.length() - text.trim().length());
+    }
+
+    @Override
+    public String toString() {
+        return "[" + name + (isMultiple() ? "List" : "") + "]";
+    }
 }
