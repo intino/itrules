f98672ec8488dc6dc79a9c350f81f50065ef1b6c
Octavio Roncal <octavioroncal11@gmail.com>
2015-10-02 08:09:59 +0100

    Fixed indentation

--- engine/src/org/siani/itrules/model/marks/Mark.java
+++ engine/src/org/siani/itrules/model/marks/Mark.java
@@ -26,73 +26,76 @@ import org.siani.itrules.model.Literal;
 import org.siani.itrules.model.Token;
 
 public class Mark extends AbstractMark {
-    private static final String NewLine = "\n";
-    private static final String OptionSeparator = "+";
+	private static final String NewLine = "\n";
+	private static final String OptionSeparator = "+";
 
-    private String name;
-    private String[] options;
-    private String separator = null;
+	private String name;
+	private String[] options;
+	private String separator = null;
 
-    public Mark(String name, String... options) {
-        this.name = name;
-        this.options = options;
-    }
+	public Mark(String name, String... options) {
+		this.name = name;
+		this.options = options;
+	}
 
-    public Mark multiple(String separator) {
-        this.separator = separator;
-        return this;
-    }
+	public Mark multiple(String separator) {
+		this.separator = separator;
+		return this;
+	}
 
-    @Override
-    public String fullName() {
-        return name + optionsText();
-    }
+	@Override
+	public String fullName() {
+		return name + optionsText();
+	}
 
-    private String optionsText() {
-        String result = "";
-        for (String option : options) result += OptionSeparator + option;
-        return result;
-    }
+	private String optionsText() {
+		String result = "";
+		for (String option : options) result += OptionSeparator + option;
+		return result;
+	}
 
-    @Override
-    public String name() {
-        return name;
-    }
+	@Override
+	public String name() {
+		return name;
+	}
 
-    @Override
-    public String separator() {
-        return separator;
-    }
+	@Override
+	public String separator() {
+		return separator;
+	}
 
-    @Override
-    public boolean isMultiple() {
-        return separator != null;
-    }
+	@Override
+	public boolean isMultiple() {
+		return separator != null;
+	}
 
-    @Override
-    public String[] options() {
-        return options;
-    }
+	@Override
+	public String[] options() {
+		return options;
+	}
 
-    @Override
-    public String indentation() {
-        return indentOf(previous);
-    }
+	@Override
+	public String indentation() {
+		return indentOf(previous);
+	}
 
-    private String indentOf(Token token) {
-        return token == null ? "" : textOf(token).isEmpty() ? indentOf(token.previous()) : textOf(token);
-    }
+	private String indentOf(Token token) {
+		return token == null ? "" : textOf(token).isEmpty() ? indentOf(token.previous()) : textOf(token);
+	}
 
-    private String textOf(Token token) {
-        return token instanceof Literal ? extractIndent(((Literal) token).text()) : "";
-    }
+	private String textOf(Token token) {
+		return token instanceof Literal ? extractIndent(((Literal) token).text()) : "";
+	}
 
-    private String extractIndent(String text) {
-        return text.contains(NewLine) ? text.substring(text.lastIndexOf(NewLine) + 1).split("[^\\s]")[0] : "";
-    }
+	private String extractIndent(String text) {
+		if (text.contains(NewLine)) {
+			final String[] split = text.substring(text.lastIndexOf(NewLine) + 1).split("[^\\s]");
+			return split.length == 0 ? "" : split[0];
+		} else return "";
+	}
 
-    @Override
-    public String toString() {
-        return "[" + name + (isMultiple() ? "..." : "") + "]";
-    }
+	@Override
+	public String toString() {
+		return "[" + name + (isMultiple() ? "..." : "") + "]";
+	}
 }
