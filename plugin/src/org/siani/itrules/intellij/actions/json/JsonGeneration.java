69b8be5dcfb8b33967192069f6f7c4e32b37acc2
Octavio Roncal <octavioroncal11@gmail.com>
2016-03-01 14:17:34 +0000

    Supported multiple generation of Java Templates

--- plugin/src/org/siani/itrules/intellij/actions/json/JsonGeneration.java
+++ plugin/src/org/siani/itrules/intellij/actions/json/JsonGeneration.java
@@ -13,41 +13,42 @@ import org.jetbrains.annotations.NotNull;
 import org.siani.itrules.intellij.actions.GenerationAction;
 
 import java.io.File;
+import java.util.List;
 
 public class JsonGeneration extends GenerationAction {
-    private static final String JSON = ".json";
+	private static final String JSON = ".json";
 
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Project project = e.getData(PlatformDataKeys.PROJECT);
-        if (projectExists(e, project)) return;
-        VirtualFile rulesFile = getVirtualFile(e);
-        if (rulesFile == null) return;
-        String title = "Generate Json";
-        if (checkDocument(project, rulesFile)) return;
-        Module moduleOf = getModuleOf(project, rulesFile);
-        File destiny = new File(findDestiny(project, moduleOf, rulesFile), rulesFile.getName() + JSON);
-        RunJsonGeneration jsonGeneration = new RunJsonGeneration(rulesFile, project, title, destiny);
-        ProgressManager.getInstance().run(jsonGeneration);
-        refreshFiles(destiny);
-        if (!jsonGeneration.getIndicator().isCanceled())
-            notify(project, rulesFile, destiny);
-        else error(project, jsonGeneration.getIndicator().getText());
-    }
+	public void actionPerformed(@NotNull AnActionEvent e) {
+		Project project = e.getData(PlatformDataKeys.PROJECT);
+		if (projectExists(e, project)) return;
+		List<VirtualFile> rulesFiles = getVirtualFile(e);
+		if (rulesFiles.isEmpty()) return;
+		for (VirtualFile rulesFile : rulesFiles) {
+			if (checkDocument(project, rulesFile)) return;
+			Module moduleOf = getModuleOf(project, rulesFile);
+			File destiny = new File(findDestiny(project, moduleOf, rulesFile), rulesFile.getName() + JSON);
+			RunJsonGeneration jsonGeneration = new RunJsonGeneration(rulesFile, project, "Generate Json", destiny);
+			ProgressManager.getInstance().run(jsonGeneration);
+			refreshFiles(destiny);
+			if (!jsonGeneration.getIndicator().isCanceled()) notify(project, rulesFile, destiny);
+			else error(project, jsonGeneration.getIndicator().getText());
+		}
+	}
 
-    private void notify(Project project, VirtualFile rulesFile, File destiny) {
-        Notifications.Bus.notify(
-                new Notification("Itrules JSON Generation", "JSON for " + rulesFile.getName() + " generated", "to " + destiny.getPath(), NotificationType.INFORMATION), project);
-    }
+	private void notify(Project project, VirtualFile rulesFile, File destiny) {
+		Notifications.Bus.notify(
+			new Notification("Itrules JSON Generation", "JSON for " + rulesFile.getName() + " generated", "to " + destiny.getPath(), NotificationType.INFORMATION), project);
+	}
 
-    private void error(Project project, String message) {
-        Notifications.Bus.notify(
-                new Notification("Itrules JSON Generation", "Error occurred during template generation", message, NotificationType.ERROR), project);
-    }
+	private void error(Project project, String message) {
+		Notifications.Bus.notify(
+			new Notification("Itrules JSON Generation", "Error occurred during template generation", message, NotificationType.ERROR), project);
+	}
 
-    protected String findDestiny(Project project, Module module, VirtualFile file) {
-        if (module == null) return project.getBasePath();
-        File res = new File(module.getModuleFile().getParent().getPath(), "res");
-        res.mkdirs();
-        return res.getPath();
-    }
+	protected String findDestiny(Project project, Module module, VirtualFile file) {
+		if (module == null) return project.getBasePath();
+		File res = new File(module.getModuleFile().getParent().getPath(), "res");
+		res.mkdirs();
+		return res.getPath();
+	}
 }
