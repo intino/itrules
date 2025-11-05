7fcbef369bf8eff041e8cebde3384f5aaab79e80
Octavio Roncal <octavio.roncal@ulpgc.es>
2025-11-05 22:19:07 +0000

    updated itrules-cli

--- itrules-cli/src/io/intino/itrules/Main.java
+++ itrules-cli/src/io/intino/itrules/Main.java
@@ -1,6 +1,7 @@
 package io.intino.itrules;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.intino.itrules.parser.ITRulesSyntaxError;
 import io.intino.itrules.template.Template;
@@ -10,6 +11,7 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 public class Main {
 
@@ -22,7 +24,7 @@ public class Main {
 			JsonObject object = new Gson().fromJson(new FileReader(args[1]), JsonObject.class);
 			File outputFile = new File(args[2]);
 			outputFile.getParentFile().mkdirs();
-			String result = new Engine(template).render(object);
+			String result = new Engine(template).render(toFrame(object));
 			Files.writeString(outputFile.toPath(), result);
 			System.out.println("File successfully generated at: " + outputFile.getAbsolutePath());
 		} catch (IOException e) {
@@ -32,6 +34,27 @@ public class Main {
 		} catch (Exception e) {
 			System.err.println("Unexpected error: " + e.getMessage());
 		}
+	}
+
+	private static Frame toFrame(JsonObject object) {
+		FrameBuilder builder = new FrameBuilder();
+		object.entrySet().forEach(entry -> {
+			if (entry.getValue().isJsonPrimitive()) {
+				if (entry.getKey().equals("_type")) builder.add(entry.getValue().getAsString());
+				else builder.add(entry.getKey(), entry.getValue().getAsString());
+			} else if (entry.getValue().isJsonArray()) {
+				List<JsonElement> list = entry.getValue().getAsJsonArray().asList();
+				if (entry.getKey().equals("_type")) list.stream().map(Object::toString).forEach(builder::add);
+				else {
+					entry.getValue().getAsJsonArray().forEach(e -> {
+						if (e.isJsonPrimitive()) builder.add(entry.getKey(), e.getAsString());
+						else builder.add(entry.getKey(), toFrame(e.getAsJsonObject()));
+					});
+				}
+			} else if (entry.getValue().isJsonObject())
+				builder.add(entry.getKey(), toFrame(entry.getValue().getAsJsonObject()));
+		});
+		return builder.toFrame();
 	}
 
 	private static void printHelp() {
